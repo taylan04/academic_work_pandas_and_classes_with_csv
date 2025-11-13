0bca6184e9435d5f44eb94403859ac5bcfe9e0b7
Taylan da Silva Gonzaga <taylan.gonzaga@al.infnet.edu.br>
2025-11-13 10:55:30 -0300

    feat(tp2): add question seven

--- Questions/Pandas/Classes/classes.py
+++ Questions/Pandas/Classes/classes.py
@@ -1,27 +1,47 @@
 #Part of it was written in Portuguese due to academic requirements.
 
 class Prato():
-    def __init__(self,nome,preco,notas):
+    def __init__(self,nome,preco):
         if len(nome) <= 2:
             raise ValueError("O nome do prato deve ter mais de 2 caracteres.")
         self.nome = nome
         if preco <= 0:
             raise ValueError("O preço do prato deve ser maior que zero.")
         self.preco = preco
-        self.notas = notas
+        self.notas = []
+
+    def adicionar_nota(self, nota):
+        if not (nota > 0 or nota < 6):
+            raise ValueError("A nota deve estar entre 1 e 5.")
+        self.notas.append(nota)
+
+    def consultar_media(self):
+        if not self.notas:
+            return None
+        return sum(self.notas) / len(self.notas)
 
     def descricao(self):
         return f"Prato: {self.nome} | Preço: {self.preco:.2f}"
     
 class Bebida():
-    def __init__(self,nome,preco,notas):
+    def __init__(self,nome,preco):
         if len(nome) <= 2:
             raise ValueError("O nome deve ter mais de 2 caracteres.")
         self.nome = nome
         if preco <= 0:
             raise ValueError("O preço da bebida deve ser maior que zero.")
         self.preco = preco
-        self.notas = notas
+        self.notas = []
+
+    def adicionar_nota(self, nota):
+        if not (nota > 0 or nota < 6):
+            raise ValueError("A nota deve estar entre 1 e 5.")
+        self.notas.append(nota)
+
+    def consultar_media(self):
+        if not self.notas:
+            return None
+        return sum(self.notas) / len(self.notas)
 
     def descricao(self):
         return f"Bebida: {self.nome} | Preço da bebida: {self.preco:.2f}"
@@ -33,6 +53,7 @@ class Restaurante():
         self.nome = nome
         self.bairro = bairro
         self.colecao = []
+        self.notas = []
 
     def adicionar_item(self, novo_item):
         if novo_item in self.colecao:
@@ -49,6 +70,19 @@ class Restaurante():
                 return item
             else:
                 return "\nItem não encontrado."
+            
+    def adicionar_nota(self, nota):
+        if not (nota > 0 or nota < 6):
+            raise ValueError("A nota deve estar entre 1 e 5.")
+        self.notas.append(nota)
+
+    def consultar_media_ponderada(self):
+        media_itens = 0
+        for item in self.colecao:
+            media_itens += len(item['notas'])
+        if not self.notas and media_itens == 0:
+            return None
+        return len(self.notas) / media_itens
         
     def listar_itens_por_categoria(self, categoria):
         for item in self.colecao:
