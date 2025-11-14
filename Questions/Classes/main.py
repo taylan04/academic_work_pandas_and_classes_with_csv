d0523089a84a7a5b185ccca44fdc8cffe63534e6
Taylan <taylansilva0402@gmail.com>
2025-11-13 22:37:07 -0300

    refactor(tp2): refactoring part of the question eigth

--- Questions/Classes/main.py
+++ Questions/Classes/main.py
@@ -1,6 +1,7 @@
 from Produtos import *
 from Cliente import *
 from Restaurante import *
+from arquivo import *
 
 def questoes_cinco_seis_sete():
     # pratos e bebidas
@@ -79,6 +80,72 @@ def questoes_cinco_seis_sete():
     print(cliente2.listar_favoritos())
 
 def questao_oito():
-    
+    def salvar_restaurantes(doc):
+        restaurantes = []
+        for item in doc:
+            restaurantes.append(f"{item[2]},{item[3]}")
+        return set(restaurantes)
+
+    def listar_restaurantes_em_objetos(lista_restaurantes):
+        restaurantes = []
+        for item in lista_restaurantes:
+            item = item.split(",")
+            restaurante = Restaurante(item[0], item[1])
+            restaurantes.append(restaurante)
+        return restaurantes
+
+    def adicionar_item(catalogo, restaurante, nome, preco, categoria):
+        for item in catalogo:
+            if (item['nome'] == nome and
+                item['preco'] == preco and
+                item['categoria'] == categoria):
+                return
+
+        catalogo.append({"restaurante": restaurante, "nome": nome, "preco": preco, "categoria": categoria})
+
+    def catalogar_pratos(doc):
+        catalogo = []
+        doc = [item for item in doc if item[5].lower() == 'prato']
+        for item in doc:
+            adicionar_item(catalogo, item[2], item[4], item[6], item[5])
+
+        return catalogo
+
+    def catalogar_bebidas(doc):
+        catalogo = []
+        doc = [item for item in doc if item[5].lower() == 'bebida']
+        for item in doc:
+            adicionar_item(catalogo, item[2], item[4], item[6], item[5])
+
+        return catalogo
+
+    '''
+    def adicionar_pratos_em_restaurantes(restaurantes, pratos):
+        aux = []
+        for r in restaurantes:
+            for d in pratos:
+                if r.name == d['restaurant']:
+                    aux.append(d)
+            r.add_items(aux)
+    '''
+
+    doc = ler_arquivo()
+    restaurantes = salvar_restaurantes(doc)
+    restaurantes = listar_restaurantes_em_objetos(restaurantes)
+    pratos = catalogar_pratos(doc)
+    bebidas = catalogar_bebidas(doc)
+
+    '''print("\n== RESTAURANTES ==")
+    for r in restaurantes:
+        print(f"{r.nome}, {r.bairro}")
+
+    print("\n== PRATOS ==")
+    for p in pratos:
+        print(f"{p}")
+
+    print("\n== BEBIDAS ==")
+    for b in bebidas:
+        print(f"{b}")'''
 
 #questoes_cinco_seis_sete()
+questao_oito()
